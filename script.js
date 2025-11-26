a71f12a243619e8bf4fd72ec1f48c8d48ec56319
Inna-chmnu <penzainna27@gmail.com>
2025-11-26 10:59:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const catcards = [
-    {name:"6", value:6,  img:"6.png"},
-    {name:"7", value:7,  img:"7.png"},
-    {name:"8", value:8,  img:"8.png"},
-    {name:"9", value:9,  img:"9.png"},
-    {name:"10",value:10, img:"10.png"},
-    {name:"J", value:2,  img:"J.png"},
-    {name:"Q", value:3,  img:"Q.png"},
-    {name:"K", value:4,  img:"K.png"},
-    {name:"A", value:11, img:"A.png"},
+    {name:"6", value:6,  img:"img/6.png"},
+    {name:"7", value:7,  img:"img/7.png"},
+    {name:"8", value:8,  img:"img/8.png"},
+    {name:"9", value:9,  img:"img/9.png"},
+    {name:"10",value:10, img:"img/10.png"},
+    {name:"J", value:2,  img:"img/J.png"},
+    {name:"Q", value:3,  img:"img/Q.png"},
+    {name:"K", value:4,  img:"img/K.png"},
+    {name:"A", value:11, img:"img/A.png"},
 ];
 
 let round = 1;
@@ -39,20 +39,29 @@ document.getElementById("generate").addEventListener("click", () => {
 
   pCardElem.src = playerCard.img;
   comuterCardElem.src = compCard.img;
-  
+
   allpeople  += playerCard.value;
   allcomuter += compCard.value;
 
   peopleOElem.textContent = allpeople;
   comuterOElem.textContent = allcomuter;
+if (allpeople > 21) {
+    resultElem.textContent = `${username} Loser!!!`;
+    round = 4; 
+    return;
+  }
 
+  if (allcomuter > 21) {
+    resultElem.textContent = "Computer Loser!!!";
+    round = 4; 
+    return;
+  }
   round++;
   numroundElem.textContent = round <= 3 ? round : 3;
 
   if (round > 3) {
     if (allpeople > allcomuter) resultElem.textContent = `${username} Winner!`;
-    else if (allpeople < allcomuter) resultElem.textContent = `Computer Winner!`;
+    else if (allpeople < allcomuter) resultElem.textContent = "Computer Winner!";
     else resultElem.textContent = "Нічия!";
   }
 });
-
